1a1e6c5e21b06c50b7cce5f36d1e165cc47e3796
RaulSanzbla <raulsanzbla@gmail.com>
2017-05-21 13:19:19 +0100

    insultgenerator
    nueva version

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -5,17 +5,15 @@ public class InsultGenerator { public String generateInsult() {
       String article = "an";
       String theInsult = "";
 try {
-  38 |
-Chapter 5: Adding Dependencies and a Database
 String databaseURL = "jdbc:postgresql://";
        databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
        databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
        String username = System.getenv("POSTGRESQL_USER");
        String password = System.getenv("PGPASSWORD");
-       Connection connection = DriverManager.getConnection(databaseURL, username,
-       password);
+       Connection connection = DriverManager.getConnection(databaseURL, username,password);
 if (connection != null) {
-String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1"; Statement stmt = connection.createStatement();
+String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1"; 
+Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(SQL);
 while (rs.next()) {
 if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) { article = "a";
@@ -28,5 +26,6 @@ if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) { article = "a";
 }
 } catch (Exception e) {
 return "Database connection problem!"; }
-return theInsult; }
+return theInsult; 
+}
 }
